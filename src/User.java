b229f717879adf61751bc73de99fb1b3a62e1b5b
ethanhfreeman <45301692+ethanhfreeman@users.noreply.github.com>
2023-03-17 18:30:31 -0700

    updated base classes

---
+++ src/User.java
@@ -0,0 +1,23 @@
+public class User {
+
+    private String uniqueID;
+    private String password;
+    
+    public String getUniqueID() {
+        return uniqueID;
+    }
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        System.out.println("Please set a real password");
+        if (password == null){
+            System.out.println("Not a real password");
+            return;
+        }
+        this.password = password;
+    }
+}
