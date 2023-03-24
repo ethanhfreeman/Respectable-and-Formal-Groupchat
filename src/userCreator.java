102a820e7fd1cd56f1482a46a23e5407ad893d54
bighunchomikey <128838535+bighunchomikey@users.noreply.github.com>
2023-03-24 10:48:59 -0700

    I added a comment to test

--- src/userCreator.java
+++ src/userCreator.java
@@ -2,5 +2,6 @@ public class userCreator {
     public static void userNamePrompt(){
         System.out.println("Please enter a unique username");
         //TODO: LOOK THROUGH ALL USER ID's TO ENSURE NO DUPLICATES
+        //First change by mikey
     }
 }
