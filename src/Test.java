8e5554b9b3654ebe071e441cadefce8cc418ee33
bighunchomikey <128838535+bighunchomikey@users.noreply.github.com>
2023-03-31 11:39:48 -0700

    Testing to see code breaks when we commit to the same file

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //testing
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("This hopefully still works after the push");
+		}
 	}
 
 }
