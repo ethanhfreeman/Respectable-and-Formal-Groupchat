f58bc114a6a987c60e7a5116a600c039a753628c
ethanhfreeman <45301692+ethanhfreeman@users.noreply.github.com>
2023-03-31 11:49:01 -0700

    updated main

--- src/Connection.java
+++ src/Connection.java
@@ -1,12 +1,10 @@
 import java.sql.*;
 public class Connection {
-        java.sql.Connection conn = null;
-        Statement stat = null;
 
         public static void Connect() throws Exception{
         try {
             String url = "jdbc:postgresql://localhost/postgres?user=postgres&password=123&ssl=false";
-            conn = DriverManager.getConnection(url);
+            java.sql.Connection conn = DriverManager.getConnection(url);
             System.out.println("Connected :)");
         } catch (Exception e) {
             e.printStackTrace();
