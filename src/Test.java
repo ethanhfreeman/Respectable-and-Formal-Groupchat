2db9ab57fbcbc03faba2f0571bfeea992eea69d0
ethanhfreeman <45301692+ethanhfreeman@users.noreply.github.com>
2023-03-31 11:39:17 -0700

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int j = 0;
+		while (j < 1) {
+			System.out.println("get rekt");
+			j++;
+		}
 //testing
 	}
 
