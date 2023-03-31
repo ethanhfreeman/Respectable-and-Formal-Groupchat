5e1ff02d3b1d0b4f85a38f3bf8110911e6c0bd4a
bighunchomikey <128838535+bighunchomikey@users.noreply.github.com>
2023-03-31 12:51:34 -0700

    Select & Main Edits
    In order to use this, you'll probably want to reference lines 11-21 which are commented out.
    
    Essentially, you'll need to specify the destination of the from "temp2" to whatever database you are wishing to connect to.
    
    The edits to the 'select()' method, were to just test the select method. Currently I don't have a table set up with the columns that are specified in the select method, so I commented out the parts that would cause errors.

--- src/Database.java
+++ src/Database.java
@@ -3,12 +3,22 @@ import java.sql.*;
 public class Database {
 
     private static java.sql.Connection c = null;
-	Statement stmt = null;
+	private static Statement stmt = null;
 
     public static void Connect() throws Exception {
         try {
-            String url = "jdbc:postgresql://localhost/postgres?user=postgres&password=123&ssl=false";
+            String url = "jdbc:postgresql://localhost/temp2?user=postgres&password=1234&ssl=false";
             //TODO change these parameters to suit your localhost username and password
+            /*
+             * Above, you need to change the "temp2" portion of the url
+             * to specify the database in which you are looking to connect to
+             * previously this was just postgres, which just connects you to postgresql.
+             * This being specified to temp2, essentially does  "\c temp2" and then the subsequent
+             * methods like 'select()' work.
+             * 
+             * This probably needs to be parameterized in the long run, as we won't always be
+             * connecting to a hard-coded destination such as "temp2"
+             */
             c = DriverManager.getConnection(url);
             System.out.println("Connected :)");
         } catch (Exception e) {
@@ -18,18 +28,18 @@ public class Database {
         }
 
     }
-	public void select(String tableName) {
+	public static void select(String tableName) {
 		try {
 			stmt = c.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from " + tableName + ";");
 			while (rs.next()) {
 				int id = rs.getInt("id");
-				String username = rs.getString("username");
-				String password = rs.getString("password");
+				String username = rs.getString("name");
+				//String password = rs.getString("age");
 				
 				System.out.println("ID: " + id);
 				System.out.println("USER: " + username);
-				System.out.println("PASS: " + password);
+				//System.out.println("PASS: " + password);
 			}
 			System.out.println("Done...");
 			rs.close();
