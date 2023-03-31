bdc01351e3773541c795e60e0ac267807bfbf7b5
bighunchomikey <128838535+bighunchomikey@users.noreply.github.com>
2023-03-31 12:06:22 -0700

    Select Method added to Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,32 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 public class Database {
-
+	Connection c = null;
+	Statement stmt = null;
+	
+	public void select(String tableName) {
+		try {
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from " + tableName + ";");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String username = rs.getString("username");
+				String password = rs.getString("password");
+				
+				System.out.println("ID: " + id);
+				System.out.println("USER: " + username);
+				System.out.println("PASS: " + password);
+			}
+			System.out.println("Done...");
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+	}
 }
