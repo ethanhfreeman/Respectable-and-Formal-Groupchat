e1c5f45e4a25df5be1d9c12325de200f893b1e0c
ethanhfreeman <45301692+ethanhfreeman@users.noreply.github.com>
2023-03-31 11:49:37 -0700

    Update Connection.java

--- src/Connection.java
+++ src/Connection.java
@@ -4,6 +4,7 @@ public class Connection {
         public static void Connect() throws Exception{
         try {
             String url = "jdbc:postgresql://localhost/postgres?user=postgres&password=123&ssl=false";
+            //change these parameters to suit your localhost username and password
             java.sql.Connection conn = DriverManager.getConnection(url);
             System.out.println("Connected :)");
         } catch (Exception e) {
