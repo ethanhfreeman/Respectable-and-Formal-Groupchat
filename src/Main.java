9a2ae7ce6461a7ab40633e6c9733ac0e3d2ea4c2
ethanhfreeman <45301692+ethanhfreeman@users.noreply.github.com>
2023-04-04 11:14:21 -0700

    cleaned up main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-
-        Database.connect("postgres");
-        Database.select("company");
+        
     }
 }
