94c11c8470c7c8662b039ee635ac289283b87d16
bighunchomikey <128838535+bighunchomikey@users.noreply.github.com>
2023-04-06 11:48:15 -0700

    Update Main.java
    Updated to have connections

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Main {
 	public static Scanner scnr = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-    	
+    	Database.connect("usersdb");
+    	Database.createTable();
         printMainMenu();
     }
     
