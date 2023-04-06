0aa00559b1c534bb540b8f44bbb9ac23e2a730df
bighunchomikey <128838535+bighunchomikey@users.noreply.github.com>
2023-04-06 11:34:12 -0700

    Update Main.java
    Partial register method done in Main.java, needs to interface with the database

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
-
+import java.util.Scanner;
 public class Main {
+	public static Scanner scnr = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        
+    	
+        printMainMenu();
+    }
+    
+    public static void printMainMenu() {
+    	System.out.println("Welcome To the Deez Nutz inc ChatRoom.");
+    	System.out.println();
+    	System.out.println("Please select from the following options:");
+    	System.out.println("(R)egister, (L)ogin, (Q)uit");
+    	System.out.println("-----------------------------------------");
+    	System.out.println();
+    	System.out.print("-");
+    	String input = scnr.nextLine().toUpperCase();
+    	
+    	if (input.equals("R")) {
+    		register();
+    	} else if (input.equals("L")) {
+    		login();
+    	} else if (input.equals("Q")) {
+    		quit();
+    	}
+    }
+    
+    public static void register() {
+    	String username = "";
+    	String password = "";
+    	
+    	System.out.println();
+    	System.out.print("Username: ");
+    	username = scnr.nextLine();
+    	System.out.print("Password: ");
+    	password = scnr.nextLine();
+    }
+    
+    public static void login() {
+    	
+    }
+    
+    public static void quit() {
+    	
     }
 }
